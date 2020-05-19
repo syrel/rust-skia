aac05783c9c9f2fd89d94eb32a2681b2d72ade07
Aliaksei Syrel <alex.syrel@gmail.com>
2020-05-19 21:35:57 +0200

    make image sync and send

--- skia-safe/src/core/image.rs
+++ skia-safe/src/core/image.rs
@@ -33,6 +33,8 @@ fn test_compression_type_naming() {
 }
 
 pub type Image = RCHandle<SkImage>;
+unsafe impl Sync for Image {}
+unsafe impl Send for Image {}
 
 impl NativeBase<SkRefCntBase> for SkImage {}
 
