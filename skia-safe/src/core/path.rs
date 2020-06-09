2c276c1fe406fe376bc63e47380d9fce6e624e8c
Aliaksei Syrel <alex.syrel@gmail.com>
2020-06-09 12:15:08 +0200

    make path sync and send

--- skia-safe/src/core/path.rs
+++ skia-safe/src/core/path.rs
@@ -181,6 +181,8 @@ impl<'a> Iterator for RawIter<'a> {
 }
 
 pub type Path = Handle<SkPath>;
+unsafe impl Sync for Path {}
+unsafe impl Send for Path {}
 
 impl NativeDrop for SkPath {
     fn drop(&mut self) {
