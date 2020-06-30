705ed3646e70b2432d0c745b6baffb15a7676d7a
Aliaksei Syrel <alex.syrel@gmail.com>
2020-06-30 10:20:19 +0200

    add Data::new_bytes to create without copy

--- skia-safe/src/core/data.rs
+++ skia-safe/src/core/data.rs
@@ -67,6 +67,11 @@ impl RCHandle<SkData> {
         Data::from_ptr(unsafe { sb::C_SkData_MakeWithCopy(data.as_ptr() as _, data.len()) })
             .unwrap()
     }
+    
+    pub fn new_bytes(data: &[u8]) -> Self {
+          Data::from_ptr(unsafe { sb::C_SkData_MakeWithoutCopy(data.as_ptr() as _, data.len()) })
+            .unwrap()
+    }
 
     /// Constructs Data from a given byte slice without copying it.
     ///
